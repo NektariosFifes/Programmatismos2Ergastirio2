5dee0acb9b51d1e2738daa3389224c46dbf0c9be
NektariosFifes <it21915@hua.gr>
2020-03-09 08:45:09 -0700

    Diorthoseis

--- main.c
+++ main.c
@@ -4,13 +4,22 @@
 #define MAXSTUDENTS 100
 
 #include <stdbool.h>
+
+//!!!!!!!!!GLOBAL DATA
+
+
  struct DataMathitis {
     int AM;
     char onoma[MAXSTUDENTS];
      int bathmos;
 };
-///DEN TO BAZO MAIN GT THE EINAI STO SCOPE TOU MAIN
+//DEN TO BAZO MAIN GT THE EINAI STO SCOPE TOU MAIN
 typedef struct DataMathitis InfoMathiti ;
+InfoMathiti info[100];
+bool brethikeTOam;
+int ThesiKapouNatoBaloANexiXORO;
+int indexx=0;
+//!!!!!!!!GLOBAL DATA
 void print_menu(){
     printf(" Choose from 1 to 4:");
     printf("1. Add new student");
@@ -18,7 +27,20 @@ void print_menu(){
     printf(" 3. Print all students");
     printf(" 4. Quit");
 }
-void add_new_student(InfoMathiti info[MAXSTUDENTS]){
+
+int find_student(int Am) {
+
+
+    for (int k = 0; k < MAXSTUDENTS; k++) {
+        if (info[k].AM == Am) {
+            brethikeTOam = true;
+            ThesiKapouNatoBaloANexiXORO = k;
+        } else {
+            ThesiKapouNatoBaloANexiXORO = indexx++;
+        }
+    }
+}
+void add_new_student(){
     int Am;
     char onomateponimo[100];
     int bathmos;
@@ -30,63 +52,47 @@ void add_new_student(InfoMathiti info[MAXSTUDENTS]){
     scanf("%d",&bathmos);
 
     int brethikeTOam = false;
-    int ThesiKapouNatoBaloANexiXORO;
-    for(int k =0 ; k<MAXSTUDENTS ; k++){
-        if(info[k].AM == Am){
-            brethikeTOam = true;
-            ThesiKapouNatoBaloANexiXORO = k;
-        }
-
-        if(info[k].AM == -1 && brethikeTOam==false){
-         ThesiKapouNatoBaloANexiXORO = k;
-        }
-
-
-    }
+     ThesiKapouNatoBaloANexiXORO = 0;
+    find_student(Am);
 
     info[ThesiKapouNatoBaloANexiXORO].AM = Am;
     info[ThesiKapouNatoBaloANexiXORO].bathmos = bathmos;
-    strcpy(info[ThesiKapouNatoBaloANexiXORO].onoma, onomateponimo);
+    for(int o=0; o< sizeof(onomateponimo);o++){
+        info[ThesiKapouNatoBaloANexiXORO].onoma[o] = onomateponimo[o];
+    }
+   //allios strcpy(info[ThesiKapouNatoBaloANexiXORO].onoma, onomateponimo);
 
 }
-void delete_student(InfoMathiti info[MAXSTUDENTS]){
+
+
+
+void delete_student(){
     int Am;
     printf("DOSE AM  \n");
     scanf("%d",&Am);
 
     bool BrethikeToPrama =false;
-    for(int j=0; j <MAXSTUDENTS ; j++) {
-
-        if(info[j].AM == Am){
-            info[j].AM = -1;
-            BrethikeToPrama = true;
-            break;
-
-        }
-    }
+    find_student(Am);
 
     if(!BrethikeToPrama){
         printf("DEn sE bRiKa");
     }
 }
-void print_students(InfoMathiti info[MAXSTUDENTS]){
- for(int i=0; i<MAXSTUDENTS ; i++){
-     if(info[i].onoma != NULL) {
-         printf("AM: %d ONOMATEPONIMO:%s Bthmos:%d ", info[i].AM, info[i].onoma, info[i].bathmos);
+void print_students(){
+ for(int i=0; i<100 ; i++){
+     if(info[i].AM != -1) {
+         printf("AM:%dONOMATEPONIMO:%sBathmos:%d ", info[i].AM, info[i].onoma, info[i].bathmos);
      }
  }
 }
 
-int find_student( int AM ){
 
-}
 int main() {
 
-        InfoMathiti info[100];
 
         //ARXIKOPOIISI
         for(int i=0; i<MAXSTUDENTS;i++){
-            info[1].AM = -1;
+            info[i].AM = -1;
         }
 
 
@@ -100,13 +106,14 @@ int main() {
             } while (choice < '1' || choice > '4');
             switch (choice) {
                 case '1':
-                    add_new_student(info);
+                    add_new_student();
                     break;
                 case '2':
-                    delete_student(info);
+                    delete_student();
                     break;
                 case '3':
-                    print_students(info);
+                    print_students();
+                   //printf("3");
                     break;
                 case '4':
                     run = 0;
@@ -118,3 +125,54 @@ int main() {
 
         return 0;
     }
+
+/*
+#include<stdio.h>
+#include<string.h>
+#define MAX 1
+struct student
+{
+    char name[20];
+    int roll_no;
+    float marks;
+};
+typedef struct student arr_studentt;
+arr_studentt arr_student[MAX];
+int i;
+
+ void go(){
+     for(i = 0; i < MAX; i++ )
+     {
+         printf("\nEnter details of student %d\n\n", i+1);
+
+         printf("Enter name: ");
+         scanf("%s", &arr_student[i].name);
+
+         printf("Enter roll no: ");
+         scanf("%d", &arr_student[i].roll_no);
+
+         printf("Enter marks: ");
+         scanf("%f", &arr_student[i].marks);
+     }
+
+
+ }
+int main()
+{
+
+   go();
+
+
+    printf("\n");
+
+    printf("Name\tRoll no\tMarks\n");
+
+    for(i = 0; i < MAX; i++ )
+    {
+        printf("%s\t%d\t%.2f\n",
+               arr_student[i].name, arr_student[i].roll_no, arr_student[i].marks);
+    }
+
+    // signal to operating system program ran fine
+    return 0;
+}*/
